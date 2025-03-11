7be6748ea88e42362697aa63585782bb9a0b8a01
KeshavCodez <darkfist.official@gmail.com>
2025-03-11 09:41:47 +0530

    Done

--- aaa.py
+++
@@ -1,9 +0,0 @@
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = interface.QueryInterface(IAudioEndpointVolume)
-# volume.GetMute()
-minVol, maxVol, _ = volume.GetVolumeRange()
-print(minVol-minVol, maxVol-minVol)
