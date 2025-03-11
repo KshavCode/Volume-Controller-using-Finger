e683e8a7d13d8b336030f0322d94b9b3f6f1b204
KeshavCodez <darkfist.official@gmail.com>
2025-03-11 10:56:40 +0530

    Real Volume Control with Hand

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ minVol, maxVol, _ = volume.GetVolumeRange()             # -63 to 0
 while True : 
     success, img = capture.read()
     h, w, c = img.shape
+    startLine = 40
+    endLine = w-40
+    lengthLine = endLine-startLine
     imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     results = mpHand2.process(imgRGB)
     if results.multi_hand_landmarks:
@@ -29,26 +32,24 @@ while True :
             for index, loc in enumerate(points.landmark) : 
                 if index==4:
                     cx, cy = int(loc.x*w), int(loc.y*h)
-                    if cx <= 20 :
-                        cx = 20
-                    if cx >= 600:
-                        cx = 600
-                    if volumeInt != int((cx/w)*100):
-                        volumeInt = int((cx/w)*100)
-                        print(volumeInt)
+                    if volumeInt != int((cx/lengthLine)*100):
+                        volumeInt = int((cx/lengthLine)*100)
+                        if volumeInt >= 100 :
+                            volumeInt = 100
+                        elif volumeInt <= 3 :
+                            volumeInt = 3
                         currentVolume = volumeInt/100*(maxVol-minVol)+minVol
                         oldVolume = volume.SetMasterVolumeLevel(currentVolume, None)
                         
-
             mpDraw.draw_landmarks(img, points, mpHand1.HAND_CONNECTIONS)
     
-    cv.putText(img, str(volumeInt), (int(volumeInt/100*w-10), h//2-20), cv.FONT_ITALIC, 1, (255,255,255), 2)
-    cv.circle(img, (int(volumeInt/100*w), h//2), 20, (255, 200, 150), cv.FILLED)
+    cv.putText(img, str(volumeInt), (int(volumeInt/100*lengthLine-10), h//2-20), cv.FONT_ITALIC, 1, (255,255,255), 2)
+    cv.circle(img, (int(volumeInt/100*lengthLine), h//2), 20, (255, 200, 150), cv.FILLED)
     endTime = time.time()
     fps:int = int(1/(endTime-startTime))
     startTime = endTime
     cv.putText(img, f"FPS: {str(fps)}", (10, 50), cv.FONT_ITALIC, 1.5, (255,255,0), 3)
-    cv.line(img, (20, h//2), (w-40, h//2), (0, 255, 250), 4)
+    cv.line(img, (startLine, h//2), (lengthLine, h//2), (0, 255, 250), 4)
     cv.imshow("Video", img)
     cv.waitKey(1)
 
